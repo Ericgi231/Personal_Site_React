51ba44e9e313c98050a16cffc32f98c40d880309
Ericgi231 <ericgi231@gmail.com>
2025-09-12 18:24:17 -0500

    Added google analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import { lazy, Suspense } from 'react';
-import { Navigate, Routes, Route } from "react-router-dom";
+import { lazy, Suspense, useEffect } from 'react';
+import { Navigate, Routes, Route, useLocation } from "react-router-dom";
+import ReactGA from "react-ga4";
 import Home from '@pages/Home';
 import NotFound from '@pages/NotFound';
 
@@ -8,29 +9,39 @@ const MoraJai = lazy(() => import("@pages/MoraJai"));
 const AnimalRaceBets = lazy(() => import("@pages/AnimalRaceBets"));
 const ApiHelp = lazy(() => import("@pages/ApiHelp"));
 
+ReactGA.initialize("G-VTTP7LRNBH");
+
 const LazyRoute = ({ Component }: { Component: React.ComponentType }) => (
   <Suspense fallback={<div></div>}>
     <Component />
   </Suspense>
 );
 
-const App = () => (
-  <>
-    <main>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/collection" />
-        <Route path="/404" element={<NotFound />} />
-        <Route path="*" element={<Navigate to="/404" replace />} />
+const App = () => {
+  const location = useLocation();
 
-        <Route path="/fileshare" element={<LazyRoute Component={FileShare} />} />
-        <Route path="/god" element={<LazyRoute Component={FileShare} />} />
-        <Route path="/help" element={<LazyRoute Component={ApiHelp} />} />
-        <Route path="/morajai" element={<LazyRoute Component={MoraJai} />} />
-        <Route path="/animal-race-bets" element={<LazyRoute Component={AnimalRaceBets} />} />
-      </Routes>
-    </main>
-  </>
-)
+  useEffect(() => {
+    ReactGA.send({ hitType: "pageview", page: location.pathname });
+  }, [location]);
+  
+  return (
+    <>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/collection" />
+          <Route path="/404" element={<NotFound />} />
+          <Route path="*" element={<Navigate to="/404" replace />} />
+
+          <Route path="/fileshare" element={<LazyRoute Component={FileShare} />} />
+          <Route path="/god" element={<LazyRoute Component={FileShare} />} />
+          <Route path="/help" element={<LazyRoute Component={ApiHelp} />} />
+          <Route path="/morajai" element={<LazyRoute Component={MoraJai} />} />
+          <Route path="/animal-race-bets" element={<LazyRoute Component={AnimalRaceBets} />} />
+        </Routes>
+      </main>
+    </>
+  )
+}
 
 export default App
